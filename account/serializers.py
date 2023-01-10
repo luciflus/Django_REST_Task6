25df0e3b5e59c1e6166892168fc4fbdcdd4cb28a
Sultan Usmanov <susmanov@hostkey.com>
2023-01-10 08:07:54 +0600

    second commit

--- account/serializers.py
+++ account/serializers.py
@@ -5,8 +5,33 @@ from rest_framework.validators import UniqueValidator
 from .models import Profile, User
 
 class ProfileSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(
+        max_length=20,
+        validators=[UniqueValidator(queryset=User.objects.all())],
+        write_only=True)
+    password = serializers.CharField(
+        max_length=20,
+        write_only=True)
+    password2 = serializers.CharField(
+        max_length=20,
+        write_only=True)
+
     class Meta:
         model = Profile
-        fields = "__all__"
-        #exclude = ['user', ]
-        read_only_fields = ['author', ]
+        # fields = "__all__"
+        exclude = ['user', ]
+
+    def create(self, validated_data):
+        user = User(username=validated_data['username'])
+        user.set_password(validated_data['password'])
+        user.save()
+        profile = Profile.objects.create(
+            name=validated_data['name'],
+            user=user
+        )
+        return profile
+
+    def validate(self, data):
+        if data['password'] != data['password2']:
+            raise serializers.ValidationError("Passwords must be identical")
+        return data
--- shop/permissions.py
+++ shop/permissions.py
@@ -4,5 +4,5 @@ class IsProfilePermission(BasePermission):
     def has_object_permission(self, request, view, obj):
         return bool(
             request.method in SAFE_METHODS or
-            request.user and request.user.is_authenticated and obj.author == request.user.author
+            request.user and request.user.is_authenticated and obj.profile == request.user.profile
         )
